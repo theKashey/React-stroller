f85ede857b5b8a705889f04c55c32b29cc33c400
Ivan Pavlov <ivanpavlovv@gmail.com>
2021-03-17 00:40:45 +0300

    chore: refactor tests

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,16 +1,22 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import {Strollable, StrollableContainer} from '../src';
+import * as React from "react";
+import { render } from "@testing-library/react";
+import { Strollable, StrollableContainer } from "../src";
 
-describe('Should render correctly', () => {
-  it('Strollable', () => {
-    const { container } = render(<Strollable/>);
+describe("Strollable", () => {
+  it("should render correctly", () => {
+    const { container } = render(<Strollable />);
 
     expect(container).toMatchSnapshot();
   });
+});
 
-  it('StrollableContainer', () => {
-   const { container } = render(<StrollableContainer>child</StrollableContainer>);
-      expect(container).toMatchSnapshot();
+
+describe("StrollableContainer", () => {
+  it("should render correctly", () => {
+    const { container } = render(
+      <StrollableContainer>child</StrollableContainer>
+    );
+
+    expect(container).toMatchSnapshot();
   });
 });
